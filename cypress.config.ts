dccbd52bc42937d96689bc694d8f9074e5285a52
Juan Esteban Caicedo Alzate <61593226+TheLordJuanes@users.noreply.github.com>
2023-04-05 21:13:35 -0500

    Implemented page object models of Book and Dashboard, and exported in index.ts (#6)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'bc2poc',
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
@@ -8,7 +9,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // modify config values examples
       // config.defaultCommandTimeout = 10000
-
+      config.video = false;
+      config.screenshotOnRunFailure = false;
       // IMPORTANT return the updated config object
       return config;
     },
